0d37eb89c9a1b7fd4d626852a93725d57e5f8223
Enis Kaan Tasoren <eniskaan98@gmail.com>
2022-10-31 00:31:15 +0000

    UI

--- Assets/Scripts/Turn System/TurnSystem.cs
+++ Assets/Scripts/Turn System/TurnSystem.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class TurnSystem : MonoBehaviour
 {
     //Turn system
+    [NonSerialized]
     public int current_turn;
     public bool p1_active;
     public bool p2_active;
@@ -22,6 +24,7 @@ public class TurnSystem : MonoBehaviour
     TMP_Text PLAYER_2_SCORE_HOLDER;
 
     //
+    [NonSerialized]
     public bool is_both_player_finish_their_turns;
 
     //Guessed air quality number
@@ -34,7 +37,13 @@ public class TurnSystem : MonoBehaviour
         current_turn = 1;
     }
 
-    //when click a province, pass turn
+    [SerializeField]
+    public GameObject POPUP_SLIDER_MENU;
+
+    //when click a province, open POPUP
+    //send information to the popup window 
+
+
     // player whoes turn first clicks and chooses the competitive province
     // -> SO turn cycle is p1 p2 , p2 p1 , p1 p2 ...
     //  ...
