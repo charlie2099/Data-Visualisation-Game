ecac3d4add22c2ebdef4f1d44bcaeb1b36b443b4
Charlie <charlie6.evans@live.uwe.ac.uk>
2022-11-05 20:43:37 +0000

    Attempting to query data from a specific record or cell using LINQ

--- Assets/Scripts/DataReaders/CSVReader.cs
+++ Assets/Scripts/DataReaders/CSVReader.cs
@@ -31,7 +31,7 @@ namespace Testing
         [Serializable]
         public class CityList
         {
-            public List<CityData> cityData;
+            public List<CityData> cityDataList;
         }
         public CityList myCityList = new CityList();
 
@@ -49,12 +49,11 @@ namespace Testing
 
             const int NUM_OF_COLUMNS = 14;
             int tableSize = dataset.Length / NUM_OF_COLUMNS - 1; // ignore first row
-            //myCityList.cityData = new CityData[tableSize];
-            myCityList.cityData = new List<CityData>(new CityData[tableSize]);
+            myCityList.cityDataList = new List<CityData>(new CityData[tableSize]);
 
-            for (int i = 0; i < tableSize; i++) // Reads column by column for each row
+            for (int i = 0; i < tableSize; i++) // Reads column by column before moving to next row
             {
-                myCityList.cityData[i] = new CityData
+                myCityList.cityDataList[i] = new CityData
                 {
                     year = int.Parse(dataset[NUM_OF_COLUMNS * (i + 1)]),
                     name = dataset[NUM_OF_COLUMNS * (i + 1) + 1],
@@ -73,9 +72,22 @@ namespace Testing
                 };
             }
 
-            int row = 1;
-            int column = 2;
-            Debug.Log("ROW: " + row + ", COLUMN: " + column + ": "+ dataset[NUM_OF_COLUMNS * (row-1 + 1) + column-1]);
+            // Prints the name of each city and it's total of homicides under the queried year and city name
+            const int YEAR = 2002;
+            const string CITYNAME = "Wiltshire";
+            foreach (var cityData in myCityList.cityDataList.Where(ctx => ctx.year == YEAR).Where(ctx => ctx.name == CITYNAME))
+            {
+                Debug.Log($"From year: {cityData.year}, City Name: {cityData.name}, Total Homicides: {cityData.totalHomicides}");
+            }
+            
+            //int row = 1;
+            //int column = 2;
+            //Debug.Log("ROW: " + row + ", COLUMN: " + column + ": " + dataset[NUM_OF_COLUMNS * row + column-1]);
+            
+            // myCityList contains every record in the dataset, where each item is a row number
+
+            /*Debug.Log("List item 0 name: " + myCityList.cityDataList[0].name);
+            Debug.Log("List item 1 name: " + myCityList.cityDataList[1].name);*/
             
             /*if (dataset.Contains("Avon"))
             {
