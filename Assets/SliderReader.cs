d7262243e3f816f06eda4db4c3c35018519c5d76
Charlie <charlie6.evans@live.uwe.ac.uk>
2022-11-07 01:01:27 +0000

    Burglary data for a city can be pulled upon being clicked

--- Assets/SliderReader.cs
+++ Assets/SliderReader.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Testing;
 using TMPro;
@@ -6,20 +7,33 @@ using UnityEngine;
 public class SliderReader : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI yearSliderText;
-    private int _selectedYear;
-    
+    private int _selectedYear = 2002;
+    private string _selectedCity;
+
+    private void OnEnable() => CitySelector.OnCitySelected += UpdateSelectedCity;
+    private void OnDisable() => CitySelector.OnCitySelected -= UpdateSelectedCity;
+
     public void UpdateSliderOutput(float sliderValue)
     {
         yearSliderText.text = "Year: " + sliderValue;
         _selectedYear = (int)sliderValue;
     }
 
+    public void UpdateSelectedCity(City city)
+    {
+        _selectedCity = city.Data.cityName;
+        
+        foreach (var cityData in CSVReader.CrimeDataset.cityDataList.Where(ctx => ctx.year == _selectedYear).Where(ctx => ctx.name == _selectedCity))
+        {
+            Debug.Log($"From year: <color=orange>{cityData.year}</color>, City Name: <color=orange>{cityData.name}</color>, Total Domestic Burglaries: <color=orange>{cityData.totalDomesticBurglaries}</color>");
+        }
+    }
+
     public void SelectCrimeCategory()
     {
-        const string CITYNAME = "Wiltshire";
-        foreach (var cityData in CSVReader.CrimeDataset.cityDataList.Where(ctx => ctx.year == _selectedYear).Where(ctx => ctx.name == CITYNAME))
+        foreach (var cityData in CSVReader.CrimeDataset.cityDataList.Where(ctx => ctx.year == _selectedYear).Where(ctx => ctx.name == _selectedCity))
         {
-            Debug.Log($"From year: {cityData.year}, City Name: {cityData.name}, Total Domestic Burglaries: {cityData.totalDomesticBurglaries}");
+            Debug.Log($"From year: <color=orange>{cityData.year}</color>, City Name: <color=orange>{cityData.name}</color>, Total Domestic Burglaries: <color=orange>{cityData.totalDomesticBurglaries}</color>");
         }
     }
 }
