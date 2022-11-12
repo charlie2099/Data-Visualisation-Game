4ca03635540ff2a844823e8b162574ed0857b9f0
Enis Kaan Tasoren <eniskaan98@gmail.com>
2022-11-12 19:09:55 +0000

    Game Logic and New Turn System, UI

--- Assets/Scripts/DataReaders/CSVRequester.cs
+++ Assets/Scripts/DataReaders/CSVRequester.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace DataReaders
 {
@@ -10,14 +12,50 @@ namespace DataReaders
         private void OnEnable() => CitySelector.OnCitySelected += DisplayCrimeDataOnCitySelect;
         private void OnDisable() => CitySelector.OnCitySelected -= DisplayCrimeDataOnCitySelect;
 
+        //Activates - Deavtives Game
+        [SerializeField] public Button Activate_Game_Button;
+        [SerializeField] public Button Activate_Visualization_Button;
+
+        //Determines if the game loop is active, if not only pull data and show it in gradient.
+        public bool Is_Game_Active;
+
+        //Canvas Controllers
+        public Canvas Visualization_Canvas;
+        public Canvas Game_Canvas;
+        public Canvas Selection_Canvas;
+
+        //Game Manager
+        public GameManager Game_Manager;
+
+        //Alert
+        [SerializeField] public Canvas AlertCanvas;
+        private void Start()
+        {
+
+            //Canvas startup
+            Visualization_Canvas.renderMode = RenderMode.WorldSpace;
+            Game_Canvas.renderMode = RenderMode.WorldSpace;
+            Selection_Canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+
+            //Button Alocations
+            Activate_Game_Button.onClick.AddListener(PressedPlayGameButton);
+            Activate_Visualization_Button.onClick.AddListener(PressedWiewVisualizationButton);
+
+            //Activation of the game is false at the start
+            Is_Game_Active = false;
+
+        }
         private void DisplayCrimeDataOnCitySelect(City city)
         {
             if (CrimeType.CrimeTypeName == null)
             {
                 Debug.LogWarning("Please select a crime category");
+                StartCoroutine(Alert());
                 return;
             }
-            
+
+
+
             foreach (var cityData in CSVReader.CrimeDataset.cityDataList.Where(ctx => ctx.year == CrimeYear.YearOfCrime).Where(ctx => ctx.name == city.Data.cityName))
             {
                 // There is probably a cleaner way to do this...
@@ -25,39 +63,87 @@ namespace DataReaders
                 {
                     case "Homicide":
                         Debug.Log($"From year: <color=orange>{cityData.year}</color>, County Name: <color=orange>{cityData.name}</color>, Total Homicides: <color=orange>{cityData.totalHomicides}</color>");
+                        if (Is_Game_Active)
+                        {
+                            Game_Manager.Player_Chooses_City_For_Game(cityData.totalHomicides, cityData.name, "Homicides", cityData.year.ToString());
+                        }
                         break;
                     case "Harrassment":
                         Debug.Log($"From year: <color=orange>{cityData.year}</color>, County Name: <color=orange>{cityData.name}</color>, Total Harassments: <color=orange>{cityData.totalHarrassments}</color>");
+                        if (Is_Game_Active)
+                        {
+                            Game_Manager.Player_Chooses_City_For_Game(cityData.totalHarrassments, cityData.name, "Harrassment", cityData.year.ToString());
+                        }
                         break;
                     case "Kidnapping":
                         Debug.Log($"From year: <color=orange>{cityData.year}</color>, County Name: <color=orange>{cityData.name}</color>, Total Kidnappings: <color=orange>{cityData.totalKidnappings}</color>");
+                        if (Is_Game_Active)
+                        {
+                            Game_Manager.Player_Chooses_City_For_Game(cityData.totalKidnappings, cityData.name, "Kidnapping", cityData.year.ToString());
+                        }
                         break;
                     case "Robbery":
                         Debug.Log($"From year: <color=orange>{cityData.year}</color>, County Name: <color=orange>{cityData.name}</color>, Total Robberies: <color=orange>{cityData.totalRobberies}</color>");
+                        if (Is_Game_Active)
+                        {
+                            Game_Manager.Player_Chooses_City_For_Game(cityData.totalRobberies, cityData.name, "Robbery", cityData.year.ToString());
+                        }
                         break;
                     case "Burglary":
                         Debug.Log($"From year: <color=orange>{cityData.year}</color>, County Name: <color=orange>{cityData.name}</color>, Total Domestic Burglaries: <color=orange>{cityData.totalDomesticBurglaries}</color>");
+                        if (Is_Game_Active)
+                        {
+                            Game_Manager.Player_Chooses_City_For_Game(cityData.totalDomesticBurglaries, cityData.name, "Burglary", cityData.year.ToString());
+                        }
                         break;
                     case "Shoplifting":
                         Debug.Log($"From year: <color=orange>{cityData.year}</color>, County Name: <color=orange>{cityData.name}</color>, Total Shoplifting: <color=orange>{cityData.totalShoplifting}</color>");
+                        if (Is_Game_Active)
+                        {
+                            Game_Manager.Player_Chooses_City_For_Game(cityData.totalShoplifting, cityData.name, "Shoplifting", cityData.year.ToString());
+                        }
                         break;
                     case "Theft":
                         Debug.Log($"From year: <color=orange>{cityData.year}</color>, County Name: <color=orange>{cityData.name}</color>, Total Theft offences: <color=orange>{cityData.totalTheft}</color>");
+                        if (Is_Game_Active)
+                        {
+                            Game_Manager.Player_Chooses_City_For_Game(cityData.totalTheft, cityData.name, "Theft", cityData.year.ToString());
+                        }
                         break;
                     case "Arson":
                         Debug.Log($"From year: <color=orange>{cityData.year}</color>, County Name: <color=orange>{cityData.name}</color>, Total Arson offences: <color=orange>{cityData.totalArson}</color>");
+                        if (Is_Game_Active)
+                        {
+                            Game_Manager.Player_Chooses_City_For_Game(cityData.totalArson, cityData.name, "Arson", cityData.year.ToString());
+                        }
                         break;
                     case "Criminal damage":
                         Debug.Log($"From year: <color=orange>{cityData.year}</color>, County Name: <color=orange>{cityData.name}</color>, Total Criminal Damage offences: <color=orange>{cityData.totalCriminalDamageOffences}</color>");
+                        if (Is_Game_Active)
+                        {
+                            Game_Manager.Player_Chooses_City_For_Game(cityData.totalCriminalDamageOffences, cityData.name, "Criminal Damage Offences", cityData.year.ToString());
+                        }
                         break;
                     case "Drug possession":
                         Debug.Log($"From year: <color=orange>{cityData.year}</color>, County Name: <color=orange>{cityData.name}</color>, Total Drug Possession offences: <color=orange>{cityData.totalDrugOffences}</color>");
+                        if (Is_Game_Active)
+                        {
+                            Game_Manager.Player_Chooses_City_For_Game(cityData.totalDrugOffences, cityData.name, "Drug Offences", cityData.year.ToString());
+                        }
                         break;
                     case "Weapon possession":
                         Debug.Log($"From year: <color=orange>{cityData.year}</color>, County Name: <color=orange>{cityData.name}</color>, Total Weapon Possession offences: <color=orange>{cityData.totalWeaponPossession}</color>");
+                        if (Is_Game_Active)
+                        {
+                            Game_Manager.Player_Chooses_City_For_Game(cityData.totalWeaponPossession, cityData.name, "Weapon Possession", cityData.year.ToString());
+                        }
                         break;
                     case "Total offences":
                         Debug.Log($"From year: <color=orange>{cityData.year}</color>, County Name: <color=orange>{cityData.name}</color>, Total Offences: <color=orange>{cityData.totalOffences}</color>");
+                        if (Is_Game_Active)
+                        {
+                            Game_Manager.Player_Chooses_City_For_Game(cityData.totalOffences, cityData.name, "Total Offences", cityData.year.ToString());
+                        }
                         break;
                 }
             }
@@ -65,5 +151,41 @@ namespace DataReaders
 
         // Enter a CrimeType (Key) to return a string (Value)
         private Dictionary<CrimeType, string> _crimeTypeDictionary;
+
+
+        private void PressedPlayGameButton()
+        {
+            //Canvas Settings
+            Visualization_Canvas.renderMode = RenderMode.WorldSpace;
+            Game_Canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            Selection_Canvas.renderMode = RenderMode.WorldSpace;
+
+            //Actiavte Game Settings
+            Is_Game_Active = true;
+        }
+
+        private void PressedWiewVisualizationButton()
+        {
+            //Canvas Settings
+            Visualization_Canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            Game_Canvas.renderMode = RenderMode.WorldSpace;
+            Selection_Canvas.renderMode = RenderMode.WorldSpace;
+            Is_Game_Active = false;
+        }
+    
+         IEnumerator Alert()
+        {
+            if (Is_Game_Active)
+            {
+                AlertCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+                yield return new WaitForSeconds(2f);
+                AlertCanvas.renderMode = RenderMode.WorldSpace;
+            }
+            yield return new WaitForSeconds(0.1f);
+        }   
     }
+
+
+
+
 }
---
+++ Assets/Scripts/Game Manager/GameManager.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Turn system
+    [NonSerialized] private int _current_turn = 1;
+
+    [NonSerialized] private bool _P1_Turn = true;
+    [NonSerialized] private bool _P2_Turn = false;
+
+    [NonSerialized] public int _P1_Score;
+    [NonSerialized] public int _P2_Score;
+
+    [NonSerialized] public int Selected_Crime_Count_Of_P1;
+    [NonSerialized] public int Selected_Crime_Count_Of_P2;
+
+
+    //UI
+    //Crime Name Holder
+    [SerializeField] public TMP_Text Crime_Name_Holder;
+
+    //Current Turn
+    [SerializeField] public TMP_Text Current_Turn_Text_Holder;
+
+    //Crime Counts
+    [SerializeField] public TMP_Text P1_Crime_Count_Text_Holder;
+    [SerializeField] public TMP_Text P2_Crime_Count_Text_Holder;
+
+    //Scores of Players
+    [SerializeField] public TMP_Text P1_Score_Text_Holder;
+    [SerializeField] public TMP_Text P2_Score_Text_Holder;
+    
+    [SerializeField] public TMP_Text P1_Selected_City;
+    [SerializeField] public TMP_Text P2_Selected_City;
+
+    //Panel Managers
+    [SerializeField] public GameObject Selection_Panel;
+    [SerializeField] public GameObject Results_Panel;
+
+    private void Start()
+    {
+        Results_Panel.SetActive(false);
+
+    }
+    private void Update()
+    {
+        //Update Current Turn
+        Current_Turn_Text_Holder.text = "ROUND " + _current_turn;
+
+        //Update Score
+        P1_Score_Text_Holder.text = _P1_Score.ToString();
+        P2_Score_Text_Holder.text = _P2_Score.ToString();
+
+    }
+
+    public void Player_Chooses_City_For_Game(int Crime_Count, String County_Name, String Crime_Name, String Date)
+    {
+        if (_P1_Turn == true && _P2_Turn == false)
+        {
+            //Pass Turn
+            _P1_Turn = false;
+            _P2_Turn = true;
+
+            //Initialize Values
+            Selected_Crime_Count_Of_P1 = Crime_Count;
+
+            //Show in UI
+            Crime_Name_Holder.text = Crime_Name + " in " + Date;
+            //
+            P1_Selected_City.text = County_Name;
+            P1_Crime_Count_Text_Holder.text = Selected_Crime_Count_Of_P1.ToString();
+
+
+        }
+        else if(_P2_Turn == true && _P1_Turn == false)
+        {
+            //Pass Turn
+            _P1_Turn = true;
+            _P2_Turn = false;
+            //Initialize Values
+            Selected_Crime_Count_Of_P2 = Crime_Count;
+            P2_Selected_City.text = County_Name;
+            P2_Crime_Count_Text_Holder.text = Selected_Crime_Count_Of_P2.ToString();
+
+            //Start End State of the Game
+            StartCoroutine(EndState());
+        }
+    }
+
+
+    [SerializeField] GameObject P1Win;
+    [SerializeField] GameObject P2Win;
+    IEnumerator EndState()
+    {
+
+
+        yield return new WaitForSeconds(0.5f);
+        
+        Selection_Panel.SetActive(false);
+        Results_Panel.SetActive(true);
+        P1Win.SetActive(false);
+        P2Win.SetActive(false);
+
+        yield return new WaitForSeconds(1f);
+        P1_Crime_Count_Text_Holder.text = Selected_Crime_Count_Of_P1.ToString();
+        yield return new WaitForSeconds(1f);
+        P2_Crime_Count_Text_Holder.text = Selected_Crime_Count_Of_P2.ToString();
+
+
+        if (Selected_Crime_Count_Of_P1 < Selected_Crime_Count_Of_P2)
+        {
+            //P1 Wins
+            P1Win.SetActive(true);
+            _P1_Score++;
+
+        }
+        else if(Selected_Crime_Count_Of_P1 > Selected_Crime_Count_Of_P2)
+        {
+            //P2 Wins
+            P2Win.SetActive(true);
+            _P2_Score++;
+
+        }
+        else if(Selected_Crime_Count_Of_P1 == Selected_Crime_Count_Of_P2)
+        {
+            //Draw
+            _P1_Score++;
+            _P2_Score++;
+
+        }
+
+        yield return new WaitForSeconds(3f);
+        _current_turn++;
+
+        Crime_Name_Holder.text = "";
+        P1_Selected_City.text = "";
+        P2_Selected_City.text = "";
+
+        Selection_Panel.SetActive(true);
+        Results_Panel.SetActive(false);
+
+
+    }
+
+
+}
